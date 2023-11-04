99fd6ac3770fab6d808fc339bf189fcdeebb6b05
Farid Mahmudov <razormad666@gmail.com>
2023-11-04 22:05:28 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,30 @@
-import {UserType} from '../HW8'
+import {initialPeople, UserType} from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[] = initialPeople, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            if(action.payload === 'up'){
+                const copy = [...state]
+                copy.sort((a,b) => a.name.localeCompare(b.name))
+                return  copy
+            }
+            else if (action.payload === 'down'){
+                const copy = [...state]
+                copy.sort((a,b) => b.name.localeCompare(a.name))
+                return  copy
+            }
+            else {
+                return  state
+            }
 
-            return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return  [...state].filter(u => u.age > action.payload )
         }
         default:
             return state
